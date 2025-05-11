e874cfeced901fae7ff4870e054ad9078a4c1939
Mounir Ybanez <rinuom91@gmail.com>
2025-05-11 13:44:46 +0800

    Added running server.

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"witwiz/proto"
+	"witwiz/server"
 
+	"google.golang.org/grpc"
+)
+
+func main() {
+	gameServer := server.NewServer()
+	grpcServer := grpc.NewServer()
+	proto.RegisterWitWizServer(grpcServer, gameServer)
+
+	shutdownSigChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownSigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-shutdownSigChan
+		log.Println("will stop serving")
+		grpcServer.GracefulStop()
+		log.Println("stopped serving")
+	}()
+
+	lis, err := net.Listen("tcp", ":40041")
+	if err != nil {
+		log.Fatalf("unable to listen at 40041: %v\n", err)
+	}
+
+	log.Println("server running at 40041")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("unable to serve at 40041: %v\n", err)
+	}
+
+	log.Println("server shutdown")
 }
--- server/server.go
+++ server/server.go
@@ -1,4 +1,11 @@
 package server
 
+import "witwiz/proto"
+
 type Server struct {
+	proto.UnimplementedWitWizServer
+}
+
+func NewServer() *Server {
+	return &Server{}
 }
